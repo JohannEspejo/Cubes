564cd8c8159a5a9be7b7acd5bd28127882da3f10
Johann Espejo <johann.espejo@hotmail.com>
2019-07-25 18:54:21 +0200

    first

---
+++ FrontEnd.fsx
@@ -0,0 +1,253 @@
+﻿namespace Solids.FrontEnd
+open System.Drawing
+open System.Windows.Forms
+open System
+open Solids.Logics
+
+module Main =
+    open Functions
+    
+    //Create labes
+    let labelC1 =
+        let temp = new Label()
+        do temp.Text<- "Coordinates Solid 1"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(20,22)
+        temp
+    
+    let labelX1 =
+        let temp = new Label()
+        do temp.Text<- "x1"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(5,40)
+        temp
+
+    let labelY1 =
+        let temp = new Label()
+        do temp.Text<- "y1"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(5,60)
+        temp
+
+    let labelZ1 =
+        let temp = new Label()
+        do temp.Text<- "z1"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(5,80)
+        temp
+
+    let labelD1 =
+        let temp = new Label()
+        do temp.Text<- "Dimensions Solid 1"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(20,102)
+        temp
+
+    let labelW1 =
+        let temp = new Label()
+        do temp.Text<- "w1"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(5,120)
+        temp
+
+    let labelL1 =
+        let temp = new Label()
+        do temp.Text<- "l1"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(5,140)
+        temp
+
+    let labelH1 =
+        let temp = new Label()
+        do temp.Text<- "h1"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(5,160)
+        temp
+
+    let labelC2 =
+        let temp = new Label()
+        do temp.Text<- "Coordinates Solid 2"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(170,22)
+        temp
+
+    let labelX2 =
+        let temp = new Label()
+        do temp.Text<- "x2"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(145,40)
+        temp
+
+    let labelY2 =
+        let temp = new Label()
+        do temp.Text<- "y2"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(145,60)
+        temp
+
+    let labelZ2 =
+        let temp = new Label()
+        do temp.Text<- "z2"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(145,80)
+        temp
+
+    let labelD2 =
+        let temp = new Label()
+        do temp.Text<- "Dimensions Solid 2"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(170,102)
+        temp
+
+    let labelW2 =
+        let temp = new Label()
+        do temp.Text<- "w2"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(145,120)
+        temp
+
+    let labelL2 =
+        let temp = new Label()
+        do temp.Text<- "l2"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(145,140)
+        temp
+
+    let labelH2 =
+        let temp = new Label()
+        do temp.Text<- "h2"
+        do temp.AutoSize <- true
+        do temp.Location <- new Drawing.Point(145,160)
+        temp
+
+    //Create textboxes 
+    let textboxX1 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(25,40)
+        temp
+ 
+    let textboxY1 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(25,60)
+        temp   
+ 
+    let textboxZ1 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(25,80)
+        temp   
+ 
+    let textboxW1 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(25,120)
+        temp
+ 
+    let textboxL1 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(25,140)
+        temp
+ 
+    let textboxH1 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(25,160)
+        temp
+
+    let textboxX2 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(165,40)
+        temp
+ 
+    let textboxY2 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(165,60)
+        temp
+ 
+    let textboxZ2 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(165,80)
+        temp   
+ 
+    let textboxW2 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(165,120)
+        temp
+ 
+    let textboxL2 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(165,140)
+        temp   
+ 
+    let textboxH2 =
+        let temp = new TextBox()
+        do temp.Text <- "0"
+        do temp.Location <- new Drawing.Point(165,160)
+        temp  
+    
+    // Create button
+    let button1 =
+        let temp = new Button()
+        do temp.Text <- "Calculate"
+        do temp.Location <- new Drawing.Point(120,200)
+        temp  
+
+    //Create Form and Add Controls   
+    let form = new Form(BackColor = Color.AntiqueWhite, Text = "Calculate Intersection Between 2 Solids", AutoSize = true)
+    form.Controls.Add(labelC1)
+    form.Controls.Add(labelX1)
+    form.Controls.Add(labelY1)
+    form.Controls.Add(labelZ1)
+    form.Controls.Add(labelD1)
+    form.Controls.Add(labelW1)
+    form.Controls.Add(labelL1)
+    form.Controls.Add(labelH1)
+    form.Controls.Add(labelC2)
+    form.Controls.Add(labelX2)
+    form.Controls.Add(labelY2)
+    form.Controls.Add(labelZ2)
+    form.Controls.Add(labelD2)
+    form.Controls.Add(labelW2)
+    form.Controls.Add(labelL2)
+    form.Controls.Add(labelH2)  
+    form.Controls.Add(textboxX1)
+    form.Controls.Add(textboxY1)
+    form.Controls.Add(textboxZ1)
+    form.Controls.Add(textboxW1)
+    form.Controls.Add(textboxL1)
+    form.Controls.Add(textboxH1)
+    form.Controls.Add(textboxX2)
+    form.Controls.Add(textboxY2)
+    form.Controls.Add(textboxZ2)
+    form.Controls.Add(textboxW2)
+    form.Controls.Add(textboxL2)
+    form.Controls.Add(textboxH2)
+    form.Controls.Add(button1)
+  
+    //Trigger function 
+    let Calculate evArgs = 
+        let Solid_1 = new Solids.Logics.Objects.Solid("Solid 1",float textboxW1.Text,float textboxL1.Text,float textboxH1.Text, float textboxX1.Text, float textboxY1.Text, float textboxZ1.Text)  
+        let Solid_2 = new Solids.Logics.Objects.Solid("Solid 2",float textboxW2.Text,float textboxL2.Text,float textboxH2.Text, float textboxX2.Text, float textboxY2.Text, float textboxZ2.Text) 
+        Solid_1.Print()
+        Solid_2.Print()
+        let MyIntersect = Functions.Intersect(Solid_1.setX, Solid_1.setY, Solid_1.setZ, Solid_2.setX, Solid_2.setY, Solid_2.setZ) 
+        if Objects.ExistIntersection = true then 
+            let InterSolid = new Solids.Logics.Objects.Solid("Intersection", Objects.InterWidth, Objects.InterLength, Objects.InterHeight, Objects.InterPointX, Objects.InterPointY, Objects.InterPointZ)            
+            InterSolid.Print()
+            MessageBox.Show(Solid_1.Values + Solid_2.Values + InterSolid.Values, Objects.ResultText) |> ignore
+        else
+            MessageBox.Show(Solid_1.Values + Solid_2.Values, Objects.ResultText) |> ignore
+    Calculate
+    //Define Action
+    button1.Click.Add(Calculate)
+
+    //Show Form
+    Application.Run(form)
---
+++ Logic.fsx
@@ -0,0 +1,179 @@
+﻿namespace Solids.Logics
+open System.Net
+open System
+open System.IO
+open Microsoft.FSharp.Control.CommonExtensions
+open NUnit.Framework
+open FsUnit
+
+module Objects =
+    /// Global variables to catch data
+    let mutable ExistIntersection = false
+    let mutable ResultText = ""
+    let mutable InterWidth = 0.0
+    let mutable InterLength = 0.0
+    let mutable InterHeight = 0.0
+    let mutable InterPointX = 0.0
+    let mutable InterPointY = 0.0
+    let mutable InterPointZ = 0.0
+
+    [<AbstractClass>]
+    /// Base class for Rectangular Solid Object 
+    type Solids() =        
+        abstract member Name : string with get
+        abstract member Width : float with get
+        abstract member Length : float with get
+        abstract member Height : float with get
+        abstract member X : float with get
+        abstract member Y : float with get
+        abstract member Z : float with get
+        
+        /// Virtual Method Volume
+        member this.Volume = this.Width * this.Length * this.Height
+
+        /// Virtual Method Coords Implementation
+       
+        abstract member Xl : float with get
+        default this.Xl = this.X - this.Width / 2.0
+        
+        abstract member Yl : float with get
+        default this.Yl = this.Y - this.Length / 2.0
+        
+        abstract member Zl : float with get
+        default this.Zl = this.Z - this.Height / 2.0
+        
+        abstract member Xu : float with get
+        default this.Xu = this.X + this.Width / 2.0
+        
+        abstract member Yu : float with get
+        default this.Yu = this.Y + this.Length / 2.0
+        
+        abstract member Zu : float with get
+        default this.Zu = this.Z + this.Height / 2.0
+
+        abstract member setX : Set<float> with get
+        default this.setX = Set.ofList [ this.Xl .. this.Xu ]
+
+        abstract member setY : Set<float> with get
+        default this.setY = Set.ofList [ this.Yl .. this.Yu ]
+
+        abstract member setZ : Set<float> with get
+        default this.setZ = Set.ofList [ this.Zl .. this.Zu ]
+
+        abstract member Values : string with get
+        default this.Values = 
+            "\r ----------------------------" +
+            "\r Name: " + (string this.Name) +
+            "\r Width: " + (string this.Width) +
+            "\r Length: " + (string this.Length) +
+            "\r Height: " + (string this.Height) +
+            "\r x: " + (string this.X) +
+            "\r y: " + (string this.Y) +
+            "\r z: " + (string this.Z) +
+            "\r Lower x point: " + (string this.Xl) +
+            "\r Lower y point: " + (string this.Yl) +
+            "\r Lower z point: " + (string this.Zl) +
+            "\r Upper x point: " + (string this.Xu) +
+            "\r Upper y point: " + (string this.Yu) +
+            "\r Upper z point: " + (string this.Zu) +
+            "\r Volume: " + (string this.Volume)
+
+        abstract member Print : unit -> unit
+        default this.Print () = 
+            printfn "-------------------------"
+            printfn "Name: %s" this.Name 
+            printfn "Width: %f" this.Width
+            printfn "Length: %f" this.Length
+            printfn "Height: %f" this.Height 
+            printfn "x: %f" this.X
+            printfn "y: %f" this.Y
+            printfn "z: %f" this.Z              
+            printfn "Xl:  %f" this.Xl
+            printfn "Xu:  %f" this.Xu
+            printfn "Yl:  %f" this.Yl
+            printfn "Yu:  %f" this.Yu
+            printfn "Yl:  %f" this.Zl
+            printfn "Yu:  %f" this.Yu
+            printfn "setX:  %b" this.setX.IsEmpty
+            printfn "setY:  %b" this.setY.IsEmpty
+            printfn "setZ:  %b" this.setZ.IsEmpty
+            printfn "Volume:  %f" this.Volume
+
+
+    /// Individual class inherit base class and overload
+    type Solid(n:string, a:float, b:float, c:float, x:float, y:float, z:float) =
+        inherit Solids()
+        override this.Name = n
+        override this.Width = a
+        override this.Length = b
+        override this.Height = c
+        override this.X = x
+        override this.Y = y
+        override this.Z = z
+
+    // Unit Test Object creation and data
+    [<TestFixture>]
+        type ``Solid_1 object test`` ()=
+            let solid1 = new Solid("solid_1", 4.0, 4.0, 4.0, 2.0, 2.0, 2.0)
+
+            [<Test>] member x.
+             ``Check if attributes are pasing to object`` ()=
+                    float solid1.Width |> should equal 4.0
+
+            [<Test>] member x.
+             ``Check if volume calculus is Ok".`` ()=
+                    solid1.Volume |> should equal 64
+    [<TestFixture>]
+        type ``Solid_2 object test`` ()=
+            let solid2 = new Solid("solid_2", 3.0, 3.0, 3.0, 3.0, 3.0, 3.0)
+
+            [<Test>] member Width.
+             ``Check if attributes are pasing to object`` ()=
+                    float solid2.Width |> should equal 4.0
+
+            [<Test>] member Volume.
+             ``Check if volume calculus is Ok".`` ()=
+                    solid2.Volume |> should equal 27.0
+
+module Functions =
+    // Set list of coords x,y,z and get intersection
+    let Intersect (setX1, setY1, setZ1, setX2, setY2, setZ2) =
+        let setInterX = Set.intersect setX1 setX2        
+        let setInterY = Set.intersect setY1 setY2       
+        let setInterZ = Set.intersect setZ1 setZ2
+        if(setInterX.IsEmpty = false && setInterY.IsEmpty = false && setInterZ.IsEmpty = false) then                         
+             do Objects.ExistIntersection <- true
+             do Objects.ResultText <- "Intersection!!!"
+             printfn "Intersection!"
+             do Objects.InterWidth <- (Set.maxElement setInterX) - (Set.minElement setInterX)
+             do Objects.InterLength <- (Set.maxElement setInterY) - (Set.minElement setInterY)
+             do Objects.InterHeight <- (Set.maxElement setInterZ) - (Set.minElement setInterZ)
+             do Objects.InterPointX <- (Set.minElement setInterX) + (Objects.InterWidth / 2.0)
+             do Objects.InterPointY <- (Set.minElement setInterY) + (Objects.InterLength / 2.0)
+             do Objects.InterPointZ <- (Set.minElement setInterZ) + (Objects.InterHeight / 2.0)
+        else 
+            do Objects.ResultText <- "No intersection..."
+            printfn "No Intersection..."
+    Intersect
+    /// Unit Test Object creation and data
+    [<Test>]
+    let ``Check getting intersection sequence``() =
+        let sX1 = Set.ofList [ 1.0 .. 3.0 ]
+        let sX2 = Set.ofList [ 2.0 .. 4.0 ]
+        let sInterX = Set.intersect sX1 sX2 
+        sInterX.IsEmpty |> should equal false
+
+    [<Test>]
+    let ``Check dimension of intersection sequence``() =
+        let sX1 = Set.ofList [ 1.0 .. 3.0 ]
+        let sX2 = Set.ofList [ 2.0 .. 4.0 ]
+        let sInterX = Set.intersect sX1 sX2 
+        sInterX.Count |> should equal 2.0
+
+    [<Test>]
+    let ``Check fiiling variable of intersection sequence``() =
+        let sX1 = Set.ofList [ 1.0 .. 3.0 ]
+        let sX2 = Set.ofList [ 2.0 .. 4.0 ]
+        let sInterX = Set.intersect sX1 sX2
+        let IWidth = (Set.maxElement sInterX) - (Set.minElement sInterX)
+        IWidth |> should equal 1.0
